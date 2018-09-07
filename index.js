cea6e1d68746e8cf80f19e19165ddda7afdcc392
Ana Harris <vana1990@gmail.com>
2018-09-07 01:24:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return 
+  return Object.assign({}, object, )
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
