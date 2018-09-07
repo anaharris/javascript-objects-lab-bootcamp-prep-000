49ce3a74bb1ae77cd7a65b2a9fcfe859b81ac768
Ana Harris <vana1990@gmail.com>
2018-09-07 01:05:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function estructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object [key] = value;
+  object[key] = value;
   return object;
 }
